ebfb5b135407a416a63bdf5e91069f2f121cee92
xREMAGIx <tramdo2806@gmail.com>
2022-01-05 03:39:15 +0700

    :hammer: enhance registerInfo route

--- src/controllers/registerInfo.ts
+++ src/controllers/registerInfo.ts
@@ -1,14 +1,27 @@
 import { asyncHandler } from '../middlewares/async';
 import { RegisterInfo } from '../models/registerInfo';
 import express, { Request, Response } from 'express'
+import mongoose from 'mongoose'
 
 // @des View all RegisterInfos
 // @route GET /api/registerInfo/
 // @access  Public
 export const getRegisterInfos = asyncHandler(async (req: Request, res: Response) => {
-    const registerInfo = await RegisterInfo.find().populate('user').populate('vaccineRegister')
+    let registerInfo;
+    if (req.params.userId) {
+        registerInfo = await RegisterInfo.find({ user: new mongoose.Types.ObjectId(req.params.userId) }).populate('user').populate('vaccineRegister').populate('previousVaccine')
+    } else {
+        registerInfo = await RegisterInfo.find().populate('user').populate('vaccineRegister').populate('previousVaccine')
+    }
+    return res.status(200).send({ status: 200, data: registerInfo })
+});
 
-    return res.status(200).send({status: 200, data: registerInfo})
+// @des View all RegisterInfo
+// @route GET /api/registerInfo/:id
+// @access  Private
+export const getRegisterInfo = asyncHandler(async (req: Request, res: Response) => {
+    const registerInfo = await RegisterInfo.findById(req.params.id).populate('user').populate('vaccineRegister').populate('previousVaccine');
+    return res.status(200).send({ status: 200, data: registerInfo })
 });
 
 // @des Create registerInfo
@@ -19,5 +32,5 @@ export const createRegisterInfo = asyncHandler(async (req: Request, res: Respons
     const registerInfo = RegisterInfo.build({ userId, typeOfRegister, vaccineRegisterId, previousVaccineId, previousVaccineDate, illnessHistory, recentSymptom, contactF0 })
     const result = await registerInfo.save()
 
-    return res.status(200).send({status: 200, data: result})
+    return res.status(200).send({ status: 200, data: result })
 });
--- src/routes/registerInfo.ts
+++ src/routes/registerInfo.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express'
 
 import { protect, roleProtect } from '../middlewares/auth'
-import { createRegisterInfo, getRegisterInfos } from '../controllers/registerInfo';
+import { createRegisterInfo, getRegisterInfos, getRegisterInfo } from '../controllers/registerInfo';
 
 const router = express.Router();
 
-router.route("/api/registerInfo").get(protect, getRegisterInfos).post(protect,createRegisterInfo);
+router.route("/api/registerInfo").get(protect, getRegisterInfos).post(protect, createRegisterInfo);
+router.route("/api/registerInfo/:id").get(protect, getRegisterInfo);
+router.route("/api/registerInfo-user/:userId").get(protect, getRegisterInfos);
 
 export { router as registerInfoRouter }
